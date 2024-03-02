e79c06205cd0f21a631ac7b2d18cf6ac69cf8f04
edwinolivares24@gmail.com <edwinolivares24@gmail.com>
2024-03-02 15:57:45 -0600

    done

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    return tutorial.split(' ').map(function (word) {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
